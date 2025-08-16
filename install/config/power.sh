62c6c0a35628988343540a7d57264c8e9f1fe840
matoval <mat24c@gmail.com>
2025-08-16 12:25:44 -0700

    removed power-profiles-daemon install since tuned-ppd (already installed on Fedora) provides the same powerprofilesctl command.

--- install/config/power.sh
+++ install/config/power.sh
@@ -2,7 +2,7 @@
 
 # Setting the performance profile can make a big difference. By default, most systems seem to start in balanced mode,
 # even if they're not running off a battery. So let's make sure that's changed to performance.
-sudo dnf install -y power-profiles-daemon
+# Note: tuned-ppd (already installed on Fedora) provides powerprofilesctl functionality
 
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
