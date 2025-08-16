dd4e5e1ecf623071c1c6fdb7d556fbe1ee3a6e8f
matoval <mat24c@gmail.com>
2025-08-16 14:03:01 -0700

    Handle when Docker is already installed

--- install/development/docker.sh
+++ install/development/docker.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Install Docker from Docker's official repository
-sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+if [ ! -f /etc/yum.repos.d/docker-ce.repo ]; then
+  sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+else
+  echo "Docker repository already configured"
+fi
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Limit log size to avoid running out of disk
 sudo mkdir -p /etc/docker
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+if [ ! -f /etc/docker/daemon.json ]; then
+  echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+else
+  echo "Docker daemon.json already exists, skipping log configuration"
+fi
 
 # Start Docker automatically
 sudo systemctl enable docker
