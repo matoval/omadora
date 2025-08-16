1c370798be1cb1c3073ef95260425c046f4b1233
matoval <mat24c@gmail.com>
2025-08-16 13:39:03 -0700

    Fix adding docker official repo

--- install/development/docker.sh
+++ install/development/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Docker from Docker's official repository
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Limit log size to avoid running out of disk
